b998b9217b800394e15d574819073da81f6ccf5c
Pankti Turakhiya <35331687+panks05@users.noreply.github.com>
2025-11-07 19:44:33 -0800

    Create ChallengeDay6.sql

---
+++ Day6/ChallengeDay6.sql
@@ -0,0 +1,12 @@
+-- For each hospital service, calculate the total number of patients admitted, total patients refused, and the admission rate (percentage of requests that were admitted). Order by admission rate descending.
+USE hospital;
+SELECT
+service,
+SUM(patients_admitted) AS total_patients_admitted,
+SUM(patients_refused) AS total_patients_refused,
+ROUND((SUM(patients_admitted) / SUM(patients_request)) * 100,2)AS admission_rate
+FROM services_weekly
+GROUP BY service
+ORDER BY admission_rate DESC;
+
+
