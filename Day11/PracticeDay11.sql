b387693c4b63a9393d219695529db250166ca1aa
Pankti Turakhiya <35331687+panks05@users.noreply.github.com>
2025-11-13 21:47:52 -0800

    Create PracticeDay11.sql

---
+++ Day11/PracticeDay11.sql
@@ -0,0 +1,15 @@
+-- List all unique services in the patients table.
+USE hospital;
+SELECT DISTINCT
+service
+FROM patients;
+
+-- Find all unique staff roles in the hospital.
+SELECT DISTINCT
+role
+FROM staff;
+
+-- Get distinct months from the services_weekly table.
+SELECT DISTINCT
+month
+FROM services_weekly;
